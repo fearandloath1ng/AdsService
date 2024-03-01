5f060a52deb1accebc9a6a52454bc0f28b9271cb
Noloquideus <daniilmanukian@gmail.com>
2024-03-02 02:59:39 +0300

    feat: creating post and account services

--- AdsService/src/Application/AdsService.Application.Contracts/IPostService.cs
+++ AdsService/src/Application/AdsService.Application.Contracts/IPostService.cs
@@ -2,5 +2,9 @@ namespace AdsService.Application.Contracts;
 
 public interface IPostService
 {
-
+    public Post CreatePost(Jwt jwt, PostContent content);
+    public PostStatus GetStatus(Jwt jwt, Guid postId);
+    public string GetRejectionReason(Jwt jwt, Guid postId);
+    public IReadOnlyCollection<Post> GetAllPostsForModeration(Jwt jwt);
+    public IReadOnlyCollection<Post> GetAllPosts(Jwt jwt);
 }
