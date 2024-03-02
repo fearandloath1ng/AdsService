717f6dd7983c6902892210c2f43482713395af89
Noloquideus <daniilmanukian@gmail.com>
2024-03-02 14:02:57 +0300

    fix: update GetRejectionReason to try method

--- AdsService/src/Application/AdsService.Application.Contracts/IPostService.cs
+++ AdsService/src/Application/AdsService.Application.Contracts/IPostService.cs
@@ -6,7 +6,7 @@ public interface IPostService
 {
     public PostDto CreatePost(Jwt jwt, PostContent content);
     public PostStatus GetStatus(Jwt jwt, Guid postId);
-    public string GetRejectionReason(Jwt jwt, Guid postId);
+    public bool TryGetRejectionReason(Jwt jwt, Guid postId, out string rejectionReason);
     public IReadOnlyCollection<PostDto> GetAllPostsForModeration(Jwt jwt);
     public IReadOnlyCollection<PostDto> GetAllPosts(Jwt jwt);
 }
